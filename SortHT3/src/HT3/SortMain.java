5d40dbdfbb2e9e2c840d499fa6515264cbb72c1c
Antonio Reyes <35511339+T0N1R@users.noreply.github.com>
2018-02-09 18:48:26 -0600

    Update SortMain.java

--- SortHT3/src/HT3/SortMain.java
+++ SortHT3/src/HT3/SortMain.java
@@ -1,4 +1,5 @@
 package HT3;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -8,7 +9,8 @@ package HT3;
 
 /**
  *
- * @author Antonio 
+ * @author Antonio
+ * @author Claude Calewart
  */
 public class SortMain {
 
@@ -18,8 +20,56 @@ public class SortMain {
     public static void main(String[] args) {
         // TODO code application logic here
         Sort main = new Sort();
-        main.generarNumeros("texto.txt");
-        //System.out.println(main.numeros[2999]);
+        
+        Scanner scan = new Scanner(System.in);
+        boolean salida = false;
+        System.out.println(main.numeros.length);
+        while(salida == false){
+            System.out.println("Ingrese la accion que se desea realizar: ");
+            System.out.println("1---Generar los numeros aleatorios");
+            System.out.println("2---Ordenar los numeros por Gnome Sort");
+            System.out.println("3---Ordenar los numeros por Bubble Sort");
+            System.out.println("4---Ordenar los numeros por Merge Sort");
+            String eleccion = scan.nextLine();
+            
+            switch(eleccion){
+                
+                case "0":{
+                    salida = true;
+                    break;
+                }
+                
+                case "1":{
+                    main.generarNumeros("texto.txt");
+                    System.out.println("Se generaron los numeros aleatorios");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("---------------------------------------");
+                    break;
+                }
+            
+                case "2":{
+                    System.out.println("numeros ordenados por GnomeSort");
+                    main.gnomeSort(main.numeros, "texto.txt");
+                    break;
+                }
+                
+                case "3":{
+                    System.out.println("Numeros ordenados por bubbleSort");
+                    main.bubbleSort(main.numeros, "texto.txt");
+                    break;
+                }
+                
+                case "4":{
+                    System.out.println("Numeros ordenados por MergeSort");
+                    main.mergeSort(main.numeros, "texto.txt");
+                }
+            }
+        }
+        
+        
+        
+        
     }
     
 }
